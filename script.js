6d773280073f1728fd77a10f86151206a04291c6
Nguyen <philippe.nguyen86@gmail.com>
2021-05-18 12:42:22 +0200

    ajout géolocalisation

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
-let villeChoisie = "bordeaux";
-recevoirTemperature(villeChoisie);
+let villeChoisie;
 
-let changerDeVille = document.querySelector('#changer');
-changerDeVille.addEventListener('click', () => {
+if("geolocation" in navigator) {
+    navigator.geolocation.watchPosition((position) => {
+        
+        const url = 'https://api.openweathermap.org/data/2.5/weather?lat='
+            + position.coords.latitude + '&lon='
+            + position.coords.longitude + '&appid=94d605aa831adf2d719da92e06cefb32&units=metric';
+        
+        let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+        requete.open('GET', url); // Nous récupérons juste des données
+        requete.responseType = 'json'; // Nous attendons du JSON
+        requete.send(); // Nous envoyons notre requête
+
+        // Dès qu'on reçoit une réponse, cette fonction est executée
+        requete.onload = function() {
+        if (requete.readyState === XMLHttpRequest.DONE) {
+            if (requete.status === 200) {
+            let reponse = requete.response;
+            // console.log(reponse);
+            let temperature = reponse.main.temp;
+            let ville       = reponse.name;
+            // console.log(temperature);
+            document.querySelector('#temperature_label').textContent = temperature;
+            document.querySelector('#ville').textContent = ville;
+            }
+            else {
+            alert('Un problème est intervenu, merci de revenir plus tard.');
+            }
+        }
+        }
+    }, erreur, options);
+    
+    var options = {
+        enableHighAccuracy: true
+    }
+    }
+    else {
+    villeChoisie = "Bordeaux";
+    recevoirTemperature(villeChoisie);
+    }
+
+    let changerDeVille = document.querySelector('#changer');
+    changerDeVille.addEventListener('click', () => {
     villeChoisie = prompt('Quelle ville souhaitez-vous voir ?');
     recevoirTemperature(villeChoisie);
     });
+
+    function erreur() {
+    villeChoisie = "Bordeaux";
+    recevoirTemperature(villeChoisie);
+    }
 
     function recevoirTemperature(ville) {
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=94d605aa831adf2d719da92e06cefb32&units=metric';
@@ -33,3 +77,4 @@ changerDeVille.addEventListener('click', () => {
         }
     }
 }
+
